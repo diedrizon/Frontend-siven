a7b85eaffc87ce6341c6cec225043180fc53968b
JiniethM <montieljironj@gmail.com>
2024-11-24 00:01:07 -0600

    :sparkles: Agregar plugins para el uso de otros sistemas

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,9 +1,7 @@
 //const String BASE_URL = 'http://192.168.250.203:8080/siven-api';
-const String BASE_URL = 'http://172.16.146.43:8080/siven-api';
-//const String BASE_URL = 'http://192.168.1.21:8080/siven-api';
+// const String BASE_URL = 'http://172.16.146.43:8080/siven-api';
+const String BASE_URL = 'http://192.168.1.9:8080/siven-api';
 //const String BASE_URL = 'https://backend-siven.onrender.com/siven-api';
-
-
 
 // Constantes de las rutas a los catalogos
 const String CATALOGOS_RED_DE_SERVICIO = '/v1/catalogo/red-servicio';
@@ -14,5 +12,3 @@ const String CATALOGOS_MEDICAMENTOS = '/v1/catalogo/medicamentos';
 const String CATALOGOS_TIPO_SEGUIMIENTO = '/v1/catalogo/tiposeguimiento';
 const String CATALOGOS_PERSONA = '/v1/catalogo/persona';
 const String CATALOGOS_USUARIO = '/v1/usuarios';
-
-
